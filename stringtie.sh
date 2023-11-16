f98f84ee28283652db23141075dd088d318774c1
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 12:08:58 -0500

    Update stringtie.sh

--- stringtie.sh
+++ stringtie.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=32
 #SBATCH --mem-per-cpu=3G
 #SBATCH --mail-type=None
-stringtie -p 32 -v --fr -o ./asemble/HEK293T/StringTie.gtf -G ~/project/stargenome/gencode.v38.annotation.gtf -l MCF7_ST ./sort/MCF7.sorted.bam
+stringtie -p 32 -v --fr -o ./asemble/samplename/StringTie.gtf -G ~/project/stargenome/gencode.v38.annotation.gtf -l input_ST ./sort/input.sorted.bam
