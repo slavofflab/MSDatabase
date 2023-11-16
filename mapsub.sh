77b8f74cedbbdec4ea4559a72930e8753f8ca077
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 12:05:31 -0500

    Update mapsub.sh

--- mapsub.sh
+++ mapsub.sh
@@ -13,7 +13,7 @@ do
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=16
 #SBATCH --mem-per-cpu=3G
 #SBATCH --mail-type=None
-STAR --runThreadN 16 --genomeDir ~/project/stargenome/hg38_idx --genomeLoad LoadAndRemove --readFilesIn ${infile} ${infile/mate1/mate2} --outFileNamePrefix ${outfile} --outFilterIntronMotifs RemoveNoncanonical --outStd BAM_Unsorted --outSAMtype BAM Unsorted --outFilterType BySJout --outReadsUnmapped Fastx --outSAMattributes All --outSAMstrandField intronMotif > ${outfile}mapped.bam
+STAR --runThreadN 16 --genomeDir ~/project/stargenome/genome --genomeLoad LoadAndRemove --readFilesIn ${infile} ${infile/mate1/mate2} --outFileNamePrefix ${outfile} --outFilterIntronMotifs RemoveNoncanonical --outStd BAM_Unsorted --outSAMtype BAM Unsorted --outFilterType BySJout --outReadsUnmapped Fastx --outSAMattributes All --outSAMstrandField intronMotif > ${outfile}mapped.bam
 
 EOF
   sbatch map.sh
