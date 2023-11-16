a86d598fdb85713f3c9964f94aa28bdcdda4cd35
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 12:07:09 -0500

    Update rmncRNA.sh

--- rmncRNA.sh
+++ rmncRNA.sh
@@ -6,5 +6,5 @@
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=8
 #SBATCH --mem-per-cpu=5G
 #SBATCH --mail-type=None
-STAR --runThreadN 8 --genomeDir ~/project/stargenome/ncrna_idx --genomeLoad LoadAndRemove --readFilesIn ./trim/MCF7_2_R1.trimmed.fastq.gz ./trim/MCF7_2_R2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix ./rmncrna/MCF7_2. --outFilterIntronMotifs RemoveNoncanonical --outStd BAM_Unsorted --outSAMtype BAM Unsorted --outFilterType BySJout --outReadsUnmapped Fastx --outSAMattributes All --outSAMattrRGline ID:su > /dev/null
+STAR --runThreadN 8 --genomeDir ~/project/stargenome/ncrna_idx --genomeLoad LoadAndRemove --readFilesIn ./trim/input_R1.trimmed.fastq.gz ./trim/input_R2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix ./rmncrna/MCF7_2. --outFilterIntronMotifs RemoveNoncanonical --outStd BAM_Unsorted --outSAMtype BAM Unsorted --outFilterType BySJout --outReadsUnmapped Fastx --outSAMattributes All --outSAMattrRGline ID:su > /dev/null
 
