98d4b81ecb9c46d79f0433607379334119be0c53
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 11:57:18 -0500

    Update cutadapt.sh

--- cutadapt.sh
+++ cutadapt.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=2
 #SBATCH --mem-per-cpu=1G
 #SBATCH --mail-type=None
-cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -m 18 -o ./trim/MCF7_3_R1.trimmed.fastq.gz -p ./trim/MCF7_3_R2.trimmed.fastq.gz ./raw/MCF7_3_R1.fastq.gz ./raw/MCF7_3_R2.fastq.gz
+cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -m 18 -o ./trim/output_R1.trimmed.fastq.gz -p ./trim/output_R2.trimmed.fastq.gz ./raw/input_R1.fastq.gz ./raw/input_R2.fastq.gz
