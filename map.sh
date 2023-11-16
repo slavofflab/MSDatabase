a04daf8ebd141e9ad843e1c990b458e898daccaa
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 12:04:29 -0500

    Update map.sh

--- map.sh
+++ map.sh
@@ -6,5 +6,5 @@
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=16
 #SBATCH --mem-per-cpu=3G
 #SBATCH --mail-type=None
-STAR --runThreadN 16 --genomeDir ~/project/stargenome/hg38_idx --genomeLoad LoadAndRemove --readFilesIn ./rmncrna/MCF7_3.Unmapped.out.mate1 ./rmncrna/MCF7_3.Unmapped.out.mate2 --outFileNamePrefix ./map/MCF7_3. --outFilterIntronMotifs RemoveNoncanonical --outStd BAM_Unsorted --outSAMtype BAM Unsorted --outFilterType BySJout --outReadsUnmapped Fastx --outSAMattributes All --outSAMstrandField intronMotif > ./map/MCF7_3.mapped.bam
+STAR --runThreadN 16 --genomeDir ~/project/stargenome/hg38_idx --genomeLoad LoadAndRemove --readFilesIn ./rmncrna/input.Unmapped.out.mate1 ./rmncrna/input.Unmapped.out.mate2 --outFileNamePrefix ./map/input. --outFilterIntronMotifs RemoveNoncanonical --outStd BAM_Unsorted --outSAMtype BAM Unsorted --outFilterType BySJout --outReadsUnmapped Fastx --outSAMattributes All --outSAMstrandField intronMotif > ./map/input.mapped.bam
 
