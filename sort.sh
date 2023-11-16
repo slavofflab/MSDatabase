b8097bbfafab2a654487377b4e942dbb0b4f259e
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 12:08:08 -0500

    Update sort.sh

--- sort.sh
+++ sort.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
 #SBATCH --mail-type=None
-samtools sort -o ./sort/MCF7_3.sorted.bam ./map/MCF7_3.mapped.bam
+samtools sort -o ./sort/output.sorted.bam ./map/input.mapped.bam
