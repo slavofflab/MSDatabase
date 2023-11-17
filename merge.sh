eb0b024396e9fd1a36b54f4e1f074530a7c4e331
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 21:43:40 -0500

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=bamsort
+#SBATCH --job-name=bammerge
 #SBATCH --out="slurm-%j.out"
 #SBATCH --partition=day
 #SBATCH --time=4:00:00
@@ -7,3 +7,4 @@
 #SBATCH --mem-per-cpu=2G
 #SBATCH --mail-type=None
 samtools merge ./sort/output_merge.bam ./sort/input_1.sorted.bam ./sort/input_2.sorted.bam ./sort/input_3.sorted.bam
+#List all bam file here
