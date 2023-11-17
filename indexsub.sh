0616724f2990c8b8c86c97d712df4a814562ba6b
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 21:50:27 -0500

    Add files via upload

---
+++ indexsub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for infile in ./sorted/*.bam
+do
+  cat>idx.sh<<EOF
+#!/bin/bash
+#SBATCH --job-name=index
+#SBATCH --out="slurm-%j.out"
+#SBATCH --partition=day
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=8
+#SBATCH --mem-per-cpu=2G
+#SBATCH --mail-type=ALL
+samtools index -@ 8 $infile
+EOF
+  sbatch idx.sh
+done
