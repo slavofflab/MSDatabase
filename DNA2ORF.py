8009251b998c96ee63a6f0d30d5f2c6e9c17e0f8
slavofflab <150952685+slavofflab@users.noreply.github.com>
2023-11-16 22:19:45 -0500

    Update DNA2ORF.py

--- DNA2ORF.py
+++ DNA2ORF.py
@@ -1,5 +1,8 @@
 #!/usr/bin//env python
 ## This script can translate transcriptome to all possible ORFs in 3 frame by stop to stop modle.
+## Please change the sample name in line 24-26
+## "sample_name_Stop_Stop.fasta" merged same peptides and should be used as database
+## "sample_name_Stop_Stop_all.fasta" can be used to find all source of specific peptide 
 ## Author: Haomiao Su
 ## Email: haomiao.su@yale.edu
 
@@ -13,29 +16,38 @@ parser = argparse.ArgumentParser(description='Translate transcriptome to all pos
 parser.add_argument('-i', '--input', dest = 'inputfile', help = 'input DNA/RNA fasta file')
 parser.add_argument('-o', '--output', dest = 'outputfile', help = 'input protein fasta file')
 args = parser.parse_args()
+pep_mini = 20
 
 
 #input_file = args.inputfile
 #output_file = args.outputfile
-input_file = "sample.fa"
-output_file = "sample_Stop_Stop.fasta"
+input_file = "sample_name.fa"
+output_file1 = "sample_name_Stop_Stop.fasta"
+output_file2 = "sample_name_Stop_Stop_all.fasta"
 
-out_list = []
-pep_col = {}
+out_list1,out_list2 = [],[]
+pep_col1,pep_col2 = {},{}
 
 for record in SeqIO.parse(input_file, "fasta"):
     for i in range(3):
         pep_list = str(record.seq[i:].translate()).split("*")
         pep_c = 1
         for pep in pep_list:
-            if len(pep)>5:
-                if pep not in pep_col:
-                    pep_col[pep] = f'{record.id}_{i}_{pep_c}'
-                    pep_c += 1
-                else:
-#                    pep_col[pep] = pep_col[pep] + f'|{record.id}_{i}_{pep_c}'
-                    pep_c += 1
+            if len(pep) >= pep_mini:
+                if "K" in pep or "R" in pep:
+                    if pep not in pep_col1:
+                        pep_col1[pep] = f'{record.id}_{i}_{pep_c}'
+                        pep_col2[pep] = f'{record.id}_{i}_{pep_c}'
+                        pep_c += 1
+                    else:
+                        pep_col2[pep] = pep_col2[pep] + f'|{record.id}_{i}_{pep_c}'
+                        pep_c += 1
 
-for ps, pid in pep_col.items():
-    out_list.append(SeqRecord(Seq(ps), id = pid, description = ""))
-SeqIO.write(out_list, output_file, "fasta")
+for ps, pid in pep_col1.items():
+    out_list1.append(SeqRecord(Seq(ps), id = pid, description = ""))
+SeqIO.write(out_list1, output_file1, "fasta")
+
+for ps, pid in pep_col2.items():
+    out_list2.append(SeqRecord(Seq(ps), id = pid, description = ""))
+SeqIO.write(out_list2, output_file2, "fasta")
+
